38d0ac88989c6f218c6f4dc4adae292dd0c9ca54
Ashutosh_Dhyani <82019070+Ashutosh1604@users.noreply.github.com>
2023-02-03 19:30:08 +0700

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer=()=> {
 
 <div className="flex justify-center items-center flex-col mt-5">
 <p className="text-white text-sm text-center ">Come join us</p>
-<p className="text-white text-sm md:text-center">info@kryptomastery.com</p>
+<p className="text-white text-sm md:text-center">info@Ethromastery.com</p>
 </div>
 
 
@@ -18,7 +18,7 @@ const Footer=()=> {
   
 <div className=" sm:w-[90%] h-[0.25px] w-full bg-gray-400 my-2"/>
 <div className="sm:w-[90%] w-full flex justify-between items-center mt-3">
-<p className="text-white text-sm text-center ">@kryptomastery 2022</p>
+<p className="text-white text-sm text-center ">@Ethromastery 2022</p>
 <p className="text-white text-sm text-center ">All rights reserverd</p>
 </div>
 
